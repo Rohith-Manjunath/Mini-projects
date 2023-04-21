d003a78f846f42f7440c9096e2c1287c8f8e0704
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-04-21 21:38:41 +0530

    Added some extra features like humidity and location condition.

--- weather app project/weather.js
+++ weather app project/weather.js
@@ -4,6 +4,9 @@ const wind=document.getElementById("wind")
 const input=document.getElementById("input")
 const fahrenheit=document.getElementById("fahrenheit")
 const weather_pic=document.getElementById("weather_image")
+const humidity=document.getElementById("humidity")
+const weather_condition=document.getElementById("weather_condition")
+const condition_pic=document.getElementById("condition_pic")
 
 const button=document.getElementById("button")
 
@@ -11,12 +14,15 @@ const button=document.getElementById("button")
 async function data(){
    await fetch(`http://api.weatherapi.com/v1/current.json?key=d42666fcd103424dabe112253232004&q=${input.value}&aqi=no`)
     .then(response => response.json())
-    .then(data=>{city.textContent=input.value 
+    .then(data=>{city.textContent=data.location.name
         degree.textContent=data.current.temp_c +"°C"
         fahrenheit.textContent=data.current.temp_f+"°F"
-        wind.textContent=data.current.wind_mph+" m/hr"
+        wind.textContent="Wind:"+data.current.wind_mph+" m/hr"
+        humidity.textContent="Humidity:"+data.current.humidity
+        weather_condition.textContent=data.current.condition.text
+        condition_pic.textContent=data.current.condition.icon
         if(data.current.temp_c >=30){
-            weather_image.src="./sunny image.png"
+            weather_image.src="./sun1.jpg"
         }
         else if(data.current.temp_c <30){
             weather_image.src="./cloudy.webp"
