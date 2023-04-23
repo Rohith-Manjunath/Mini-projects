36d55312dca2d9f8e003ba98da418583ebb3da9a
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-04-24 02:09:58 +0530

    Improved User experience by adding more js features.

--- Pop Up project/popup.js
+++ Pop Up project/popup.js
@@ -3,7 +3,14 @@ const popup=document.getElementById("popup")
 
 button.addEventListener("click",()=>{
 
-    popup.style.transform=`translateY(10px)`;
+    popup.style.transform=`translateY(15px)`;
+
+    setTimeout(()=>{
+        popup.style.transform=`translateY(-230px)`;
+        button.style.opacity=1;
+
+
+    },2000)
     button.style.opacity=0;
     popup.style.transition='0.5s';
 
