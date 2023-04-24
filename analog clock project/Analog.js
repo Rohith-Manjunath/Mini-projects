5ad2c536d5e881fecc48eb5546f68c1fe4953698
Rohith-Manjunath <rohithmanjunath20@gmail.com>
2023-04-24 14:06:54 +0530

    Finished Analog clock project

---
+++ analog clock project/Analog.js
@@ -0,0 +1,24 @@
+let seconds_needle=document.getElementById("seconds_needle")
+let hour_needle=document.getElementById("hour_needle")
+let minutes_needle=document.getElementById("minutes_needle")
+
+
+function updateClock(){
+    const date=new Date()
+
+
+    const hour =date.getHours()
+    const mins =date.getMinutes()
+    const secs =date.getSeconds()
+const hour_Deg=(hour/12)*360;
+const sec_Deg=(secs/60)*360;
+const min_Deg=(mins/60)*360;
+hour_needle.style.transform=`rotate(${hour_Deg}deg)`
+minutes_needle.style.transform=`rotate(${min_Deg}deg)`
+seconds_needle.style.transform=`rotate(${sec_Deg}deg)`
+
+    console.log(hour,mins,secs)
+}
+
+
+setInterval(updateClock,1000)
